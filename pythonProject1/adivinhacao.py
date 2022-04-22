cc8b21658b6d1ddbd38532412cd5d0b61aa1a241
Leandro Grimaldi <leandro.grimaldi@gmail.com>
2022-04-22 12:15:02 -0300

    Adicionando 5 tentativas ao jogo.

--- pythonProject1/adivinhacao.py
+++ pythonProject1/adivinhacao.py
@@ -1,17 +1,24 @@
 print("Bem vindo ao Jogo!")
 
 numero_secreto = 42
+max_tentativas = 5
+tentativa = 1
+while(tentativa <= max_tentativas):
+    mensagem = "Tentativa " + str(tentativa) + "/5 Digite seu numero: "
+    chute = int(input(mensagem))
 
-chute = int(input("Digite seu numero: "))
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+    if(acertou):
+        print("Voce Acertou!")
+        break
+    else:
+        if(maior):
+            print("Errou, o chute foi maior que o numero secreto")
+        elif(menor):
+            print("Errou, o chute foi menor que o numero secreto")
+    tentativa = tentativa + 1
 
-if(acertou):
-    print("Voce Acertou!")
-else:
-    if(maior):
-        print("Errou, o chute foi maior que o numero secreto")
-    elif(menor):
-        print("Errou, o chute foi menor que o numero secreto")
+print("Fim da Linha")
